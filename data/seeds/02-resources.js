a4d4adfd5f1d3146a210e27cb861768a54317a37
Trenten Grede <trenten.grede@gmail.com>
2021-03-19 11:41:20 -0500

    seed files written and db seeded with base values for api to test

---
+++ data/seeds/02-resources.js
@@ -0,0 +1,17 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('resources').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('resources').insert([
+        {resource_name: 'resource 1', resource_description: 'this is the first resource'},
+        {resource_name: 'resource 2', resource_description: 'this is the second resource'},
+        {resource_name: 'resource 3', resource_description: 'this is the third resource'},
+        {resource_name: 'resource 4', resource_description: 'this is the fourth resource'},
+        {resource_name: 'resource 5', resource_description: 'this is the fifth resource'},
+        {resource_name: 'resource 6', resource_description: 'this is the sixth resource'},
+        {resource_name: 'resource 7', resource_description: 'this is the seventh resource'}
+      ]);
+    });
+};
---
+++ data/seeds/03-tasks.js
@@ -0,0 +1,19 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('tasks').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('tasks').insert([
+        {task_description: 'task A', task_notes: 'this is a task', task_completed: false, project_id: 1},
+        {task_description: 'task B', task_notes: 'this is another task', task_completed: false, project_id: 1},
+        {task_description: 'task C', task_notes: 'this is a task again', task_completed: false, project_id: 2},
+        {task_description: 'task D', task_notes: 'this is yet again a task', task_completed: false, project_id: 2},
+        {task_description: 'task E', task_notes: 'this is a task labeled e', task_completed: false, project_id: 2},
+        {task_description: 'task F', task_notes: 'this is the a test task', task_completed: false, project_id: 3},
+        {task_description: 'task G', task_notes: 'this is a task a task a task', task_completed: false, project_id: 3},
+        {task_description: 'task H', task_notes: 'this is the a ksat', task_completed: false, project_id: 1},
+        {task_description: 'task I', task_notes: 'this is the the last seeded task', task_completed: false, project_id: 2}
+      ]);
+    });
+};
