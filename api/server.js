a3e0d91160460db9f25952d70a4120ee78b831ab
Trenten Grede <trenten.grede@gmail.com>
2021-03-19 11:15:16 -0500

    db migration set up and ran for initial DB setup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
 // build your server here and require it from index.js
+const express = require('express')
+const server = express()
+
+server.use(express.json())
+
+
+
+
+
+server.use('*', (req,res) => {
+  res.send('<h1>welcome!</h1>')
+})
+
+module.exports = server
